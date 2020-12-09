032b1e3cc669ed11f72b55f4cc32923bd94909c4
JagjiwanChimkar <jagji333@gmail.com>
2020-12-09 14:10:25 +0530

    Added Background

--- src/components/NewsCards/NewsCards.js
+++ src/components/NewsCards/NewsCards.js
@@ -14,41 +14,31 @@ function NewsCards({ articles,activeArticle }) {
 
   if(!articles.length){
     return(
-    <Grow in>
+      <Grow in>
       <Grid className={classes.container} container alignItems="stretch" spacing={3}>
-           {infoCards.map((infoCard)=>(
-                <Grid item className={classes.infoCard} xs={12} sm={6} md={4} lg={3}>
-                     <div className={classes.card} style={{backgroundColor: infoCard.color}}>
-                      <Typography variant="h5">{infoCard.title}</Typography>
-                      { infoCard.info ?(
-                         <Typography variant="h6">
-                           <strong>
-                            {infoCard.title.split(' ')[2]}:
-                          </strong>
-                            <br/>
-                            {infoCard.info}
-                        </Typography>):null
-                      }
-                      <Typography variant="h6" component="h6">Try saying: <br /> <i>{infoCard.text}</i></Typography>
-                     </div>
-                </Grid>
-           ))}
+        {infoCards.map((infoCard) => (
+          <Grid item xs={12} sm={6} md={4} lg={3} className={classes.infoCard}>
+            <div className={classes.card} style={{ backgroundColor: infoCard.color }}>
+              <Typography variant="h5" component="h5">{infoCard.title}</Typography>
+              {infoCard.info ? <Typography variant="h6" component="h6"><strong>{infoCard.title.split(' ')[2]}</strong>: <br />{infoCard.info}</Typography> : null}
+              <Typography variant="h6" component="h6">Try saying: <br /> <i>{infoCard.text}</i></Typography>
+            </div>
+          </Grid>
+        ))}
       </Grid>
-  </Grow>
+    </Grow>
     );
   }
   return (
-    <div>
-      <Grow in>
-        <Grid className={classes.container} container alignItems="stretch" spacing={3}>
-          {articles.map((article, i) => (
-            <Grid key={i} className={classes.container} item xs={12} sm={6} md={4} lg={3} style={{ display: 'flex' }}>
-              <NewsCard article={article} activeArticle={activeArticle} i={i} />
-            </Grid>
-          ))}
-        </Grid>
-      </Grow>
-    </div>
+    <Grow in>
+      <Grid className={classes.container} container alignItems="stretch" spacing={3}>
+        {articles.map((article, i) => (
+          <Grid item xs={12} sm={6} md={4} lg={3} style={{ display: 'flex' }}>
+            <NewsCard activeArticle={activeArticle} i={i} article={article} />
+          </Grid>
+        ))}
+      </Grid>
+    </Grow>
   );
 }
 
